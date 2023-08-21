59641e956bf0b3ec031bbae6a8e0433b7b5c4bc5
Tim Hanewich <tahanewich@live.com>
2023-08-21 13:30:51 -0400

    Upgraded ENS160

--- src/ENS160.py
+++ src/ENS160.py
@@ -1,9 +1,25 @@
+"""
+ENS160.py MicroPython driver for the ENS160 Digital Metal-Oxide Multi-Gas Sensor manufactured by ScioSense - https://www.sciosense.com/products/environmental-sensors/ens160-digital-multi-gas-sensor/
+Author Tim Hanewich, github.com/TimHanewich
+Version 1.2, August 21, 2023
+Find updates to this code: https://github.com/TimHanewich/MicroPython-Collection/blob/master/ENS160/ENS160.py
+
+MIT License
+Copyright 2023 Tim Hanewich
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+
 import machine
+import time
 
 class ENS160:
     """
     Lightweight class for communicating with an ENS160 air quality sensor via I2C.
     Follows the specifications defined in the official ENS160 data sheet: https://www.mouser.com/datasheet/2/1081/SC_001224_DS_1_ENS160_Datasheet_Rev_0_95-2258311.pdf
+    Newer version of ENS160 data sheet: https://www.sciosense.com/wp-content/uploads/documents/SC-001224-DS-9-ENS160-Datasheet.pdf
     """
     
     def __init__(self, i2c:machine.I2C, address:int = 0x53):
@@ -60,6 +76,22 @@ class ENS160:
         5 = Unhealthy
         """
         return self.i2c.readfrom_mem(self.address, 0x21, 1)[0]
+    
+    def reset(self) -> None:
+        """Resets and returns to standard operating mode (2)"""
+
+        # write reset bytes
+        self.i2c.writeto_mem(self.address, 0x10, 0xF0)
+        time.sleep(2.0)
+
+        # go to deep sleep mode (off)
+        self.operating_mode = 0
+        time.sleep(2.0)
+
+        # go to sensing mode
+        self.operating_mode = 2
+        time.sleep(2.0)
+
     
         
     def _translate_pair(self, high:int, low:int) -> int:
