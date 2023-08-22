46e6b128311332d7984ac8346bb869385ee10259
Tim Hanewich <tahanewich@live.com>
2023-08-21 22:30:13 -0400

    Reorganized

--- src/main.py
+++ src/main.py
@@ -8,18 +8,8 @@ import AHT21
 import ENS160
 import settings
 
-
-# set up
-led = machine.Pin("LED", machine.Pin.OUT)
-i2c = machine.I2C(1, sda=machine.Pin(14), scl=machine.Pin(15))
-aht = AHT21.AHT21(i2c)
-ens = ENS160.ENS160(i2c)
-ens.reset()
-time.sleep(0.5)
-ens.operating_mode = 2
-time.sleep(2.0)
-
 # boot pattern
+led = machine.Pin("LED", machine.Pin.OUT)
 print("Playing LED boot pattern...")
 led.on()
 time.sleep(0.5)
@@ -33,6 +23,15 @@ led.on()
 time.sleep(0.5)
 led.off()
 time.sleep(0.5)
+
+# set up
+i2c = machine.I2C(1, sda=machine.Pin(14), scl=machine.Pin(15))
+aht = AHT21.AHT21(i2c)
+ens = ENS160.ENS160(i2c)
+ens.reset()
+time.sleep(0.5)
+ens.operating_mode = 2
+time.sleep(2.0)
 
 # connect to wifi
 wlan = network.WLAN(network.STA_IF)
