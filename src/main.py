de92bbba7b43f338253fb1b0cf84ab892af6a081
Tim Hanewich <tahanewich@live.com>
2024-05-20 18:52:38 -0400

    Implemented watchdog timer

--- src/main.py
+++ src/main.py
@@ -48,6 +48,11 @@ print("Connected to wifi!")
 my_ip:str = str(wlan.ifconfig()[0])
 print("My IP Address: " + my_ip)
 
+# create watchdog timer
+wdt = machine.WDT(timeout=8388) # 8,388 ms is the limit (8.388 seconds)
+wdt.feed()
+print("Watchdog timer now activated.")
+
 # Enter infinite loop
 while True:
 
@@ -56,6 +61,7 @@ while True:
     aqi:int = ens.AQI
     eco2:int = ens.CO2
     tvoc:int = ens.TVOC
+    wdt.feed()
     
     # take reading from AHT21
     print("Taking AHT21 measurements...")
@@ -63,17 +69,24 @@ while True:
     humidity:float = rht[0]
     humidity = humidity / 100
     temperature:float = rht[1]
+    wdt.feed()
 
     # create json body
     body = {"aqi": aqi, "eco2": eco2, "tvoc": tvoc, "humidity": humidity, "temperature": temperature}
     print("Measurements taken! " + str(body))
+    wdt.feed()
     
     # make HTTP call
     print("Making HTTP call...")
+    wdt.feed()
     pr = urequests.post(settings.post_url, json=body)
+    wdt.feed()
     pr.close()
     print("HTTP call made!")
 
-    # wait
-    print("Now waiting for " + str(settings.sample_time_seconds) + " seconds...")
-    time.sleep(settings.sample_time_seconds)
+    # wait for time
+    next_loop:int = time.ticks_ms() + (1000 * settings.sample_time_seconds)
+    while (time.ticks_ms() < next_loop):
+        print("Sampling next in " + str(round((next_loop - time.ticks_ms()) / 1000), 0) + " seconds...")
+        time.sleep(1)
+        wdt.feed()
